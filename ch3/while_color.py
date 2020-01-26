d3e7feac957e7ae7582b4acdc0ae6412443b1264
Jay <jay.mm.message@gmail.com>
2020-01-26 16:37:18 +0800

    實做 guest color for while.

---
+++ ch3/while_color.py
@@ -0,0 +1,9 @@
+
+color = 'blue'
+guess = ''
+guesses = 0
+
+while color != guess:
+    guess = input('What color am I thinking of? ')
+    guesses = guesses + 1
+print('You got it! It took you', guess, guesses)
