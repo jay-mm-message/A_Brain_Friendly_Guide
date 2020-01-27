d105019b36c0936cbb03a66e97d27c1cd5eb63bd
Jay <jay.mm.message@gmail.com>
2020-01-27 18:14:08 +0800

    實做 smoothies + for + range.

---
+++ ch4/smoothies_for_range.py
@@ -0,0 +1,9 @@
+
+
+smoothies = [ 'coconut', 'strawberry', 'banana', 'tropical', 'acai berry' ]
+
+#for smoothie in smoothies:
+#    print('We serve: ', smoothie)
+
+for i in range(len(smoothies)):
+    print('smoothies[', str(i), '] = ', smoothies[i])
