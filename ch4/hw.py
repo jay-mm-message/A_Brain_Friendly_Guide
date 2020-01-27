59b1b50a02fa2ba6f01ab3b9b57d0ae1bdd2b679
Jay <jay.mm.message@gmail.com>
2020-01-27 15:43:48 +0800

    新增串列實做

---
+++ ch4/hw.py
@@ -0,0 +1,17 @@
+
+eighties = [ '', 'duran duran', 'B-52s', 'muse' ]
+newwave = [ 'flock of seagulls', 'postal service' ]
+
+remember = eighties[1]
+
+eighties[1] = 'culture club'
+
+hand = newwave[0]
+
+eighties[3] = hand
+
+eighties[0] = eighties[2]
+
+eighties[2] = remember
+
+print(eighties)
