64938aa79137f3b0ebf276498dc31f69c376e4c1
Jay <jay.mm.message@gmail.com>
2020-01-28 15:23:57 +0800

    可以將 function 的參數加入初始值 模型 2

---
+++ ch5/function_init_2.py
@@ -0,0 +1,6 @@
+
+def greet(message="Where have you been?", name="Jill", emoticon="thumbs up"):
+    print(message, ", ", name, ", ", emoticon)
+
+greet()
+greet(message="Yo!", name = "Bill", emoticon=':)')
