58fe3823aa710729e206b744e46e99ea6e1945cc
Jay <jay.mm.message@gmail.com>
2020-01-28 17:49:02 +0800

    泡泡排序法, 每執行一次泡泡會將最大值浮到某一邊, 模組 2

---
+++ ch5/bubble_sort_2.py
@@ -0,0 +1,17 @@
+
+strings = [ 'coconut', 'strawberry', 'banana', 'pineapple' ]
+
+print("bubble_sort before: ", strings)
+
+swapped = True
+
+while swapped == True:
+    swapped = False
+    for i in range(0, len(strings) - 1):
+        if strings[i] > strings[i+1]:
+            tmp = strings[i]
+            strings[i] = strings[i+1]
+            strings[i+1] = tmp
+            swapped = True
+
+print("bubble_sort after: ", strings)
