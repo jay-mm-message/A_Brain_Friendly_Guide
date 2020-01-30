81cc61a119b44b4bca4aed2e23807f413b1472ca
Jay <jay.mm.message@gmail.com>
2020-01-30 14:05:29 +0800

    建立通訊錄採用 List.

---
+++ ch8/address_book.py
@@ -0,0 +1,7 @@
+
+
+names = [ 'Kim', 'John', 'Josh' ]
+emails = [ 'Kim@oreilly.com', 'john@abc.com', 'josh@wickedlysmart.com' ]
+
+for i in range (0, len(names), 1):
+    print("Address Book No. : ", i + 1 , ", name: ", names[i], ", email: ", emails[i])
