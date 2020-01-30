06ba11adb10c797899ad1871c699f0bff4f27b92
Jay <jay.mm.message@gmail.com>
2020-01-30 16:10:55 +0800

    List 可以放入一個 dictionary.

---
+++ ch8/list_bind_dictionary.py
@@ -0,0 +1,17 @@
+movies = []
+movie = {}
+
+movie['name'] = 'Forbidden Planet'
+movie['year'] = 1957
+movie['rating'] = '*****'
+movie['year'] = 1957
+
+movies.append(movie)
+
+print("movies: ", movies, ", type: ", type(movies))
+print("movie: ", movie, ", type: ", type(movie))
+
+print()
+print("movies.append(movie)")
+print("movies length: ", len(movies))
+print("movie: length: ", len(movie))
