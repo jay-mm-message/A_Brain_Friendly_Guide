68fc716112cb46265a346d6447eff6189291d04d
Jay <jay.mm.message@gmail.com>
2020-01-31 22:59:03 +0800

    實做 python exception 模組 2

---
+++ ch9/exception_open_op_2.py
@@ -0,0 +1,12 @@
+
+try:
+    num = input('Go a number:')
+    result = 42 / int(num)
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+except ValueError:
+    print("Excuse me, we asked for a number.")
+else:
+    print("Your answer is", result)
+finally:
+    print("Thanks for stopping by.")
