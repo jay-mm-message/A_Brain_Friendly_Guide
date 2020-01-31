3a1eeec8db0b1ac87c1fd62bd74d928b063110ff
Jay <jay.mm.message@gmail.com>
2020-01-31 20:53:28 +0800

    客漏字填空

---
+++ ch9/crazy_access_answser_2.py
@@ -0,0 +1,85 @@
+
+verb_ans = ['buying', 'forgetting', 'eating']
+noun_ans = ['pudding', 'monkey', 'pizza']
+adjective_ans = ['crazy']
+
+verb_num= 0
+adj_num = 0
+n_num = 0
+
+def main_crazy_lib_origin(filename):
+    my_file = open(filename, 'r')
+
+    text = ''
+    for line in my_file:
+        text = text + process_line_origin(line)
+
+    my_file.close()
+
+    return text
+def main_crazy_lib(filename):
+    my_file = open(filename, 'r')
+
+    text = ''
+    for line in my_file:
+        text = text + process_line(line)
+
+    my_file.close()
+
+    return text
+
+def process_line_origin(line):
+    return line
+
+def process_line(line):
+
+    words = line.split();
+
+    symbol = ''
+    process_line_word = ''
+
+    global verb_num
+    global adj_num
+    global n_num
+
+    for word in words:
+
+        stripped = word.strip(",.:!?")
+        if stripped != word:
+            symbol = word[-1]
+            tmp = stripped
+            word = tmp
+
+        if word in 'VERB_ING':
+            word = verb_ans[verb_num]
+            verb_num = verb_num + 1
+        if word in 'NOUN':
+            word = noun_ans[n_num]
+            n_num = n_num + 1
+        if word in 'ADJECTIVE':
+            word = adjective_ans[adj_num]
+            adj_num = adj_num + 1
+
+        process_line_word = process_line_word  + ' ' + str(word) + str(symbol)
+        symbol = ''
+
+    process_line_word = process_line_word + str('\n')
+    return process_line_word
+
+def main():
+    print("Q: ")
+    lib = main_crazy_lib_origin('lib.txt')
+    print(lib)
+
+    print("Answer:")
+    print("Noun: ", noun_ans)
+    print("Verb: ", verb_ans)
+    print("Adj: ", adjective_ans)
+    print()
+
+    lib = main_crazy_lib('lib.txt')
+    print(lib)
+    print("ver: ", verb_num, "noun_ans: ", n_num, "adj: ", adj_num)
+
+if __name__ == '__main__':
+    main()
