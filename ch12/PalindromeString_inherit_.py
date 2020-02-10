c0c285749047afef6416865773797fccd397b0fa
Jay <jay.mm.message@gmail.com>
2020-02-10 23:24:04 +0800

    繼承 string 實做 is_palindrome.

---
+++ ch12/PalindromeString_inherit_.py
@@ -0,0 +1,21 @@
+class PalindromeString(str):
+    def is_palindrome(self):
+        heat = 0
+        tail = len(self) - 1
+
+        while heat <= tail:
+            if self[heat] != self[tail]:
+                return False
+
+            heat = heat + 1
+            tail = tail - 1
+
+        return True
+
+word = input("Give a word to check palindrome: ")
+palindrome = PalindromeString(word)
+
+if palindrome.is_palindrome():
+    print(word, ", it's palindrome.")
+else:
+    print(word, ", it isn't palindrome.")
